fd7fee01b1f7ead8c695edc737e115d07f051890
Kasper <kasper.eloranta.@tuni.fi>
2021-12-06 18:11:12 +0200

    preliminary implementation of task3

--- src/main/scala/assignment21/assignment.scala
+++ src/main/scala/assignment21/assignment.scala
@@ -27,6 +27,7 @@ import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.feature.StringIndexer
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.ml.clustering.{KMeans, KMeansSummary}
 
 
@@ -78,7 +79,8 @@ object assignment  {
   
   val indexer = new StringIndexer().setInputCol("LABEL").setOutputCol("mappedLABEL")
   val dataK5D3WithLabels = indexer.fit(dataK5D2).transform(dataK5D2)
-                  
+  dataK5D3WithLabels.show()
+  
   def task1(df: DataFrame, k: Int): Array[(Double, Double)] = {
     val kdf = df.select("a","b")
       
@@ -132,7 +134,37 @@ object assignment  {
   }
 
   def task3(df: DataFrame, k: Int): Array[(Double, Double)] = {
-    ???
+    
+    val mappedDf = df.select("a","b","mappedLABEL")
+    val vectorAssembler = new VectorAssembler().setInputCols(Array("a","b","mappedLABEL"))
+                                               .setOutputCol("features")
+    val transformationPipeline = new Pipeline().setStages(Array(vectorAssembler))
+    val pipeLine = transformationPipeline.fit(mappedDf)
+    val transformedData = pipeLine.transform(mappedDf)
+        
+    val scaler = new MinMaxScaler()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+    val scalerModel = scaler.fit(transformedData)
+    val scaledData = scalerModel.transform(transformedData)
+    val kmeans = new KMeans().setK(k).setSeed(1L).setFeaturesCol("scaledFeatures")
+    val model = kmeans.fit(scaledData) 
+    
+    //val sc = spark.sparkContext
+    val clusterTuples = model.clusterCenters //.map(v => (v(0),v(1),v(2)))
+    clusterTuples.foreach(println)    
+    //val mat: RowMatrix = new RowMatrix(clusters)
+    
+    val mostFatalClusters: Array[(Double,Double)] = new Array[(Double, Double)](2);
+
+    
+    // ALUStAVA TOTEUTUS, TOIMII OIKEIN KOSKA DATA SUOTUISAA
+    for(i <- 0 to (clusterTuples.length-1)){
+      if(clusterTuples(i)(2) == 1.0){
+        mostFatalClusters :+ (clusterTuples(i)(0),clusterTuples(i)(1))
+      }
+    }
+    return mostFatalClusters
   }
 
   // Parameter low is the lowest k and high is the highest one.
